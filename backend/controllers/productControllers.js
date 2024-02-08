c8e23cc3b54ae9bcf687f0a28292f5cc3230e055
Jarek Wyprzal <jwyprzal@gmail.com>
2024-02-08 15:54:48 +0000

    add delete product endpoint

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -26,3 +26,31 @@ export const getProductDetails = async (req, res) => {
 
   res.status(200).json({ product })
 }
+
+// Update product -> /api/v1/products/:id
+export const updateProduct = async (req, res) => {
+  let product = await Product.findById(req?.params?.id)
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' })
+  }
+
+  product = await Product.findByIdAndUpdate(req?.params?.id, req?.body, {
+    new: true,
+  })
+
+  res.status(200).json({ product })
+}
+
+// Delete product -> /api/v1/products/:id
+export const deleteProduct = async (req, res) => {
+  const product = await Product.findById(req?.params?.id)
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' })
+  }
+
+  await product.deleteOne()
+
+  res.status(200).json({ message: 'Product is deleted' })
+}
