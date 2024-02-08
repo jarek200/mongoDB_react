bfaee72ee614dedefd795f83bdb41bb9ae53d6e7
Jarek Wyprzal <jwyprzal@gmail.com>
2024-02-08 15:39:51 +0000

    create single product api

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -15,3 +15,14 @@ export const newProduct = async (req, res) => {
     product,
   })
 }
+
+// Get single product details -> /api/v1/products/:id
+export const getProductDetails = async (req, res) => {
+  const product = await Product.findById(req?.params?.id)
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' })
+  }
+
+  res.status(200).json({ product })
+}
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getProducts, newProduct } from '../controllers/productControllers.js'
+import { getProducts, newProduct, getProductDetails } from '../controllers/productControllers.js'
 
 const router = express.Router()
 
 router.route('/products').get(getProducts)
 router.route('/admin/products').post(newProduct)
+router.route('/products/:id').get(getProductDetails)
 
 export default router
