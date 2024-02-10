ebe9de60af208b4eb7d5fde18d2047429ac98aab
Jarek Wyprzal <jwyprzal@gmail.com>
2024-02-10 19:39:16 +0000

    generate forgot password token

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -19,6 +19,8 @@ export const getProducts = catchAsyncErrors(async (req, res) => {
 
 // Crate a new product -> /api/v1/admin/products
 export const newProduct = catchAsyncErrors(async (req, res) => {
+  req.body.user = req.user.id
+
   const product = await Product.create(req.body)
 
   res.status(200).json({
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,10 +4,10 @@ import { isAuthenticatedUser, autorizeRoles } from '../middlewares/auth.js'
 
 const router = express.Router()
 
-router.route('/products').get(isAuthenticatedUser, autorizeRoles('admin'), getProducts)
+router.route('/products').get(getProducts)
 router.route('/products/:id').get(getProductDetails)
-router.route('/admin/products').post(newProduct)
-router.route('/admin/products/:id').put(updateProduct)
-router.route('/admin/products/:id').delete(deleteProduct)
+router.route('/admin/products').post(isAuthenticatedUser, autorizeRoles('admin'), newProduct)
+router.route('/admin/products/:id').put(isAuthenticatedUser, autorizeRoles('admin'), updateProduct)
+router.route('/admin/products/:id').delete(isAuthenticatedUser, autorizeRoles('admin'), deleteProduct)
 
 export default router
