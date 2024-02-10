b7d3962128eac9a595bc6fa496bbe5f5e4fbfb15
Jarek Wyprzal <jwyprzal@gmail.com>
2024-02-10 19:23:38 +0000

    add authorize user roles and permissions

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { getProducts, newProduct, getProductDetails, updateProduct, deleteProduct } from '../controllers/productControllers.js'
-import { isAuthenticatedUser } from '../middlewares/auth.js'
+import { isAuthenticatedUser, autorizeRoles } from '../middlewares/auth.js'
 
 const router = express.Router()
 
-router.route('/products').get(isAuthenticatedUser, getProducts)
-router.route('/admin/products').post(newProduct)
+router.route('/products').get(isAuthenticatedUser, autorizeRoles('admin'), getProducts)
 router.route('/products/:id').get(getProductDetails)
-router.route('/products/:id').put(updateProduct)
-router.route('/products/:id').delete(deleteProduct)
+router.route('/admin/products').post(newProduct)
+router.route('/admin/products/:id').put(updateProduct)
+router.route('/admin/products/:id').delete(deleteProduct)
 
 export default router
